f181cf2710b8518a89dafdab626d10709f54f896
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-29 10:07:04 -0500

    added middleware and correctly pathed * and /notes

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const PORT = 3001;
 const app = express();
  
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
-
+app.get(' *', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.get('/notes', (req, res) => {
-
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('/api/notes', (req, res) => {
