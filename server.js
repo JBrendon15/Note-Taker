cdfc56cd54ac74d0e89e2e6d6a4d906ab8ed5495
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-29 09:51:26 -0500

    added template for routes and the listen function

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const PORT = 3001;
+const app = express();
+ 
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+
+});
+
+app.get('/notes', (req, res) => {
+
+});
+
+app.get('/api/notes', (req, res) => {
+
+});
+
+app.post('/notes', (req, res) => {
+
+});
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
